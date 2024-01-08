352f2e38a6a7282b99ca957575cd5d3174e67214
Daniel Unknown <danielunknown@Daniels-MacBook-Pro.local>
2024-01-08 14:21:38 -0500

    render err

--- controllers/database.js
+++ controllers/database.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://127.0.0.1/Users')
+        await mongoose.connect('mongodb://localhost:27017/Users')
         console.log('connected to db');
     }catch(error){
        console.log(error)
